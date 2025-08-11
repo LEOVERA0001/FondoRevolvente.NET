62d0d810ee35f8d590e46dbcf9acba1c82f84312
LEOVERA0001 <leowork2024@gmail.com>
2025-08-11 15:17:55 -0600

    PRIMER INGRESO

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Fondo_Revolvente;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
